d8714ef1393a93390211de52904167980c9ae169
ARIJIT KUNDU <112720802+ARIJITKUNDU06@users.noreply.github.com>
2025-02-28 16:08:47 +0530

    Create Setting.py

---
+++ Setting.py
@@ -0,0 +1,19 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), 'db.sqlite3'),
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'users',
+    'documents',
+]
+
+AUTH_USER_MODEL = 'users.CustomUser'
